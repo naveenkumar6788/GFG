c83e8f3b89d6ca9ab5f012517b3dca3ce10cb758
naveenkumar6788 <147487898+naveenkumar6788@users.noreply.github.com>
2026-08-09 11:22:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Linked List Insertion At Beginning/linked-list-insertion-at-beginning.java
@@ -0,0 +1,20 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public Node insertAtFront(Node head, int x) {
+        // code here
+        Node newnode=new Node(x);
+        newnode.next=head;
+        return newnode;
+    }
+}
